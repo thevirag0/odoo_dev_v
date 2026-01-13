ffeb2a0e35c010b826d7ae7dc4c66b316632c533
Valeria <valberroc@alu.edu.gva.es>
2026-01-13 09:57:08 +0100

    Cambios 13/01

--- data/addons/gestion_restaurante_valeria/models/models.py
+++ data/addons/gestion_restaurante_valeria/models/models.py
@@ -1,7 +1,6 @@
- from odoo import models, fields, api
+from odoo import models, fields, api
 
-
- class gestion_restaurante_valeria(models.Model):
+class gestion_restaurante_valeria(models.Model):
     _name = 'gestion_restaurante_valeria.gestion_restaurante_valeria'
     _description = 'gestion_restaurante_valeria.gestion_restaurante_valeria'
 
@@ -13,7 +12,7 @@ def _value_pc(self):
     for record in self:
         record.value2 = float(record.value) / 100
 
- class platos_valeria(models.Model):
+class platos_valeria(models.Model):
     _name = 'gestion_restaurante_valeria.platos_valeria'
     _description = 'Modelo de Platos para Gestión de Restaurante'
 
