068dbd3b080d25bc67194692f1df237ac90e2be0
Valeria <valberroc@alu.edu.gva.es>
2026-01-30 13:06:06 +0100

    commit clase 30/01

--- data/addons/gestion_restaurante_valeria/models/models.py
+++ data/addons/gestion_restaurante_valeria/models/models.py
@@ -1,5 +1,6 @@
 from odoo import models, fields, api
 
+<<<<<<< HEAD
 
 class gestion_restaurante_valeria(models.Model):
 class gestion_restaurante_valeria(models.Model):
@@ -17,9 +18,20 @@ class gestion_restaurante_valeria(models.Model):
             record.value2 = float(record.value) / 100
 
 
+=======
+>>>>>>> c96603b (commit clase 30/01)
 class platos_valeria(models.Model):
     _name = 'gestion_restaurante_valeria.platos_valeria'
-    _description = 'Modelo de Platos para Gestión de Restaurante'
+    _description = 'gestion_restaurante_valeria.lo de los platos'
 
     name = fields.Char()
-    description = fields.Text()
+    description = fields.Text(
+    )
+
+    # @api.depends('name')
+    # def _value_pc(self):
+    #     for record in self:
+    #         if not record.description: 
+    #             record.description = record.name + "_Mi descripcion"
+            # record.value2 = float(record.value) /100
+
